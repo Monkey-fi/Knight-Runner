9e2fea5bd7b1cd984dda7bef03b9acdaaac55e95
Monkey-fi <sparsh2312049gmail.com>
2024-11-10 00:47:52 +0530

    First Scene Completed; added logo,icon,splash screen to game build

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,8 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
-    
+    public void FirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void SparshScene()
+    {
+        SceneManager.LoadScene(1);
+    } 
+
+    public void AryanScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void HarshitScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void HomeButton()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Score()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
